7f40941626d7f54e2b248bc5ddab665151813703
Joel Myers <joeldmyers@gmail.com>
2024-04-25 20:50:48 -0400

    ui exercise

--- App.tsx
+++ App.tsx
@@ -2,20 +2,41 @@ import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 const App = () => {
     return <SafeAreaView style={styles.container}>
-        <Text style={styles.headerText}>Hello Dorothee!</Text>
+        <View style={styles.header}><Text style={styles.buttonText}>Here are some boxes of different colors</Text></View>
+        <View style={[styles.buttonDiv, { backgroundColor: '#2aa198'}]}><Text style={styles.buttonText}>Cyan</Text></View>
+        <View style={[styles.buttonDiv, { backgroundColor: '#268bd2'}]}><Text style={styles.buttonText}>Blue</Text></View>
+        <View style={[styles.buttonDiv, { backgroundColor: '#d33682'}]}><Text style={styles.buttonText}>Magenta</Text></View>
+        <View style={[styles.buttonDiv, { backgroundColor: '#cb4b16'}]}><Text style={styles.buttonText}>Orange</Text></View>
     </SafeAreaView>
 }
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: "blue",
+      backgroundColor: "white",
       alignItems: 'center',
-      justifyContent: "center",
+      justifyContent: 'flex-start',
+      margin: 20
+    },
+    header: {
+        marginTop: 30
     },
     headerText: {
         fontSize: 64,
         color: '#202020'
+    },
+    buttonDiv: {
+        padding: 20,
+        width: '100%',
+        marginVertical: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+
+    },
+    buttonText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#fff',
     }
   });
 
