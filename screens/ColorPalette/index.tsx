2f408f1a2b6d799f821990ea58b874859b365c98
Joel Myers <joeldmyers@gmail.com>
2024-04-28 16:30:43 -0400

    light reorganizing

--- screens/ColorPalette/index.tsx
+++ screens/ColorPalette/index.tsx
@@ -1,79 +1,8 @@
 import { FlatList, SafeAreaView, View, Text, StyleSheet } from "react-native";
 import ColorBox from "../../components/ColorBox";
-
-type Color = {
-  colorName: string;
-  hexCode: string;
-};
+import { FIRST_COLORS } from "./consts";
 
 const ColorPalette = () => {
-  const COLORS: Color[] = [
-    {
-      colorName: "Base03",
-      hexCode: "#002b36",
-    },
-    {
-      colorName: "Base02",
-      hexCode: "#073642",
-    },
-    {
-      colorName: "Base01",
-      hexCode: "#586e75",
-    },
-    {
-      colorName: "Base00",
-      hexCode: "#657b83",
-    },
-    {
-      colorName: "Base0",
-      hexCode: "#839496",
-    },
-    {
-      colorName: "Base1",
-      hexCode: "#93a1a1",
-    },
-    {
-      colorName: "Base2",
-      hexCode: "#eee8d5",
-    },
-    {
-      colorName: "Base3",
-      hexCode: "#fdf6e3",
-    },
-    {
-      colorName: "Yellow",
-      hexCode: "#b58900",
-    },
-    {
-      colorName: "Orange",
-      hexCode: "#cb4b16",
-    },
-    {
-      colorName: "Red",
-      hexCode: "#dc322f",
-    },
-    {
-      colorName: "Magenta",
-      hexCode: "#d33682",
-    },
-    {
-      colorName: "Violet",
-      hexCode: "#6c71c4",
-    },
-    {
-      colorName: "Blue",
-      hexCode: "#268bd2",
-    },
-    {
-      colorName: "Cyan",
-      hexCode: "#2aa198",
-    },
-    {
-      colorName: "Green",
-      hexCode: "#859990",
-    },
-  ];
-
   return (
     <>
       <SafeAreaView style={styles.container}>
@@ -84,7 +13,7 @@ const ColorPalette = () => {
         </View>
         <FlatList
           style={{ width: "100%" }}
-          data={COLORS}
+          data={FIRST_COLORS}
           keyExtractor={(item) => item.colorName}
           renderItem={(data) => (
             <ColorBox
---
+++ screens/ColorPalette/types.d.ts
@@ -0,0 +1,4 @@
+export type Color = {
+  colorName: string;
+  hexCode: string;
+};
