892b4d3ba7c3d604a7071066fa54a22ae32fde5a
Joel Myers <joeldmyers@gmail.com>
2024-04-28 17:21:53 -0400

    color box preview working solution

--- components/PalettePreview/index.tsx
+++ components/PalettePreview/index.tsx
@@ -1,4 +1,10 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+} from "react-native";
 import { Props } from "./types";
 import { FC } from "react";
 
@@ -7,6 +13,16 @@ const PalettePreview: FC<Props> = ({ handlePress, colorPalette }) => {
     <View>
       <TouchableOpacity onPress={handlePress}>
         <Text style={styles.linkText}>{colorPalette.paletteName}</Text>
+        <FlatList
+          data={colorPalette.colors.slice(0, 5)}
+          keyExtractor={(item) => item.colorName}
+          renderItem={({ item }) => (
+            <View
+              style={[styles.colorBox, { backgroundColor: item.hexCode }]}
+            />
+          )}
+          horizontal
+        />
       </TouchableOpacity>
     </View>
   );
@@ -16,6 +32,15 @@ const styles = StyleSheet.create({
   linkText: {
     fontSize: 24,
     padding: 10,
+  },
+  colorBox: {
+    height: 30,
+    width: 30,
+    marginHorizontal: 3,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 1,
   },
 });
 
